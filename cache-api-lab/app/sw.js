de31311db8564f7983384f15658912f61d259c0f
Tony Eneh <aeneh90@gmail.com>
2019-12-25 02:23:16 +0100

    Intercept fetch and respond from cache

--- cache-api-lab/app/sw.js
+++ cache-api-lab/app/sw.js
@@ -1,3 +1,5 @@
+self.skipWaiting();
+
 const filesToCache = [
     '/',
     'style/main.css',
@@ -15,6 +17,28 @@ self.addEventListener('install', event => {
         caches.open(staticCacheName)
         .then(cache => {
             return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            if (response) {
+                console.log('Found ', event.request.url, ' in cache');
+                return response;
+            }
+            console.log('Network request for ', event.request.url);
+            return fetch(event.request)
+
+            // TODO 4 - Add fetched files to the cache
+
+        }).catch(error => {
+
+            // TODO 6 - Respond with custom offline page
+
         })
     );
 });
