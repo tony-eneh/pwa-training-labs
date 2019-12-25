5a648b2018958762b21061d8e4a1b4fa2d55041e
Tony Eneh <aeneh90@gmail.com>
2019-12-25 01:30:51 +0100

    Intercept fetch requests with service worker

--- service-worker-lab/app/service-worker.js
+++ service-worker-lab/app/service-worker.js
@@ -1,3 +1,5 @@
-self.addEventListener('install', console.log `Service worker installed`);
-self.addEventListener('activate', console.log `Service worker activated`);
+self.addEventListener('install', () => console.log `Service worker installed`);
+self.addEventListener('activate', () => console.log `Service worker activated`);
 self.skipWaiting();
+
+self.addEventListener('fetch', (event) => console.log `fetching... ${event.request.url}`)
